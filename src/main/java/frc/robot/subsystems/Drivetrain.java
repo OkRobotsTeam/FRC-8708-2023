b9b93e64cde29f9a4abee80094a6d8988ca98062
ftc7078 <59035211+ftc7078@users.noreply.github.com>
2023-02-26 16:13:17 -0500

    fixed rev pneumatics

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,6 +4,7 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticHub;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -22,13 +23,17 @@ public class Drivetrain extends SubsystemBase{
     private final MotorControllerGroup m_rightMotors = new MotorControllerGroup(m_rightMotor1, m_rightMotor2);
 
     private final DifferentialDrive m_diffDrive = new DifferentialDrive(m_leftMotors, m_rightMotors);
-
-    private final DoubleSolenoid m_solenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
+    private final PneumaticHub m_pneumaticHub = new PneumaticHub();
+    private final DoubleSolenoid m_solenoid = new DoubleSolenoid(22, PneumaticsModuleType.REVPH, 0, 1);
 
     public Drivetrain() {
         m_rightMotors.setInverted(true);
 
     }
+    public void init() {
+        m_pneumaticHub.enableCompressorDigital();
+        m_solenoid.set(Value.kForward);
+     }
 
     public void tankDrive(double leftSpeed, double rightSpeed, boolean fast) {
         if (fast) {
