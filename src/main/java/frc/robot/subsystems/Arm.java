762aa25439aad126d3a9ffd1c5be5b3706431462
mysticallego <mysticallego@gmail.com>
2023-02-26 16:48:40 -0500

    Added encoders and elevator to arm subsystem

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,19 +1,35 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
 public class Arm extends SubsystemBase{
 
     private final DoubleSolenoid m_piston = new DoubleSolenoid(PneumaticsModuleType.CTREPCM,2,3);
 
     private final CANSparkMax m_elbow = new CANSparkMax(5,MotorType.kBrushless);
+    private final RelativeEncoder m_elbowEncoder = m_elbow.getEncoder();
 
+    private final CANSparkMax m_elevator1 = new CANSparkMax(6, MotorType.kBrushless);
+    private final CANSparkMax m_elevator2 = new CANSparkMax(7, MotorType.kBrushless);
+    private final MotorControllerGroup m_elevator = new MotorControllerGroup(m_elevator1, m_elevator2);
+    private final RelativeEncoder m_elevatorEncoder = m_elevator1.getEncoder();
+
+    
     public Arm() {}
+
+    public void init() {
+        m_elevator1.setIdleMode(IdleMode.kBrake);
+        m_elevator2.setIdleMode(IdleMode.kBrake);
+    }
 
     @Override
     public void periodic() {}
