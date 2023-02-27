2446dba05ad4e7192a0b80dac720603c3775a704
deekb <derek.m.baier@gmail.com>
2023-02-27 08:19:50 -0500

    Changed solenoid type to REVPH in Arm.java, added motor ports to Constants

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.CANEncoder;
+import frc.robot.Constants.ArmConstants;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMax.IdleMode;
@@ -13,13 +14,13 @@ import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
 public class Arm extends SubsystemBase{
 
-    private final DoubleSolenoid m_piston = new DoubleSolenoid(PneumaticsModuleType.CTREPCM,2,3);
+    private final DoubleSolenoid m_piston = new DoubleSolenoid(PneumaticsModuleType.REVPH, ArmConstants.kArmRaiseChannel, ArmConstants.kArmLowerChannel);
 
-    private final CANSparkMax m_elbow = new CANSparkMax(5,MotorType.kBrushless);
+    private final CANSparkMax m_elbow = new CANSparkMax(ArmConstants.kElbowMotorPort, MotorType.kBrushless);
     private final RelativeEncoder m_elbowEncoder = m_elbow.getEncoder();
 
-    private final CANSparkMax m_elevator1 = new CANSparkMax(6, MotorType.kBrushless);
-    private final CANSparkMax m_elevator2 = new CANSparkMax(7, MotorType.kBrushless);
+    private final CANSparkMax m_elevator1 = new CANSparkMax(ArmConstants.kElevatorMotorPort1, MotorType.kBrushless);
+    private final CANSparkMax m_elevator2 = new CANSparkMax(ArmConstants.kElevatorMotorPort2, MotorType.kBrushless);
     private final MotorControllerGroup m_elevator = new MotorControllerGroup(m_elevator1, m_elevator2);
     private final RelativeEncoder m_elevatorEncoder = m_elevator1.getEncoder();
 
@@ -27,6 +28,7 @@ public class Arm extends SubsystemBase{
     public Arm() {}
 
     public void init() {
+        // Ensure that if we stop sending commands to the elevator it stays put
         m_elevator1.setIdleMode(IdleMode.kBrake);
         m_elevator2.setIdleMode(IdleMode.kBrake);
     }
