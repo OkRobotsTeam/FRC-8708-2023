49ae4b0fc4c3367398cf38283fe2b9378e9912ab
mysticallego <mysticallego@gmail.com>
2023-02-27 15:54:13 -0500

    Added set and get methods for elevator and piston

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -6,6 +6,7 @@ import frc.robot.Constants.PneumaticsConstants;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMax.SoftLimitDirection;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
@@ -25,15 +26,69 @@ public class Arm extends SubsystemBase{
     private final MotorControllerGroup m_elevator = new MotorControllerGroup(m_elevator1, m_elevator2);
     private final RelativeEncoder m_elevatorEncoder = m_elevator1.getEncoder();
 
+    private boolean elevatorExtended = false;
     
-    public Arm() {}
-
-    public void init() {
-        // Ensure that if we stop sending commands to the elevator it stays put
+    public Arm() {
         m_elevator1.setIdleMode(IdleMode.kBrake);
         m_elevator2.setIdleMode(IdleMode.kBrake);
+
+        m_elevator1.setSmartCurrentLimit(1);
+        m_elevator2.setSmartCurrentLimit(1);
+
+        m_elevator1.enableSoftLimit(SoftLimitDirection.kForward, true);
+        m_elevator1.enableSoftLimit(SoftLimitDirection.kReverse, true);
+        m_elevator2.enableSoftLimit(SoftLimitDirection.kForward, true);
+        m_elevator2.enableSoftLimit(SoftLimitDirection.kReverse, true);
+
+        m_elevator1.setSoftLimit(SoftLimitDirection.kForward, (float)ArmConstants.kElevatorExtendRotations);
+        m_elevator1.setSoftLimit(SoftLimitDirection.kReverse, 0);
+        m_elevator2.setSoftLimit(SoftLimitDirection.kForward, (float)ArmConstants.kElevatorExtendRotations);
+        m_elevator2.setSoftLimit(SoftLimitDirection.kReverse, 0);
+
+        m_elevatorEncoder.setPosition(0);
+    }
+
+    private void updateElevatorSpeed() {
+        if (elevatorExtended) {
+            m_elevator.set(ArmConstants.kMaxElevatorSpeed);
+        } else {
+            m_elevator.set(-ArmConstants.kMaxElevatorSpeed);
+        }
+    }
+
+    public boolean getElevatorExtended() {
+        if (m_elevatorEncoder.getPosition() > 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void setElevatorExtended(boolean isExtended) {
+        if (getPistonRaised()) {
+            elevatorExtended = isExtended;
+        } else {
+            elevatorExtended = false;
+        }
+    }
+
+    public boolean getPistonRaised() {
+        if (m_piston.get() == PneumaticsConstants.kArmRaise) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void setPistonRaised(boolean isRaised) {
+        if (getElevatorExtended()) {
+            setElevatorExtended(false);
+        }
+        m_piston.set(PneumaticsConstants.kArmRaise);
     }
 
     @Override
-    public void periodic() {}
+    public void periodic() {
+        updateElevatorSpeed();
+    }
 }
