ec879953de0ede2400d80a54d6f2b0a8a42f6f1e
deekb <derek.m.baier@gmail.com>
2023-03-03 10:29:35 -0500

    randomfixes

--- src/main/java/frc/robot/subsystems/Lights.java
+++ src/main/java/frc/robot/subsystems/Lights.java
@@ -9,6 +9,7 @@ public class Lights extends SubsystemBase {
     public static final double yellow = 0.69;
     public static final double off = 0.99;
     public static final double redchaser = -0.31;
+    public static final double bluechaser = 
 
     public final PWMSparkMax LED = new PWMSparkMax(0);
 
