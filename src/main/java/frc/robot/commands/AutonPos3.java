6ef6828b28ce8835d75cde497ff89d1f229c2921
mysticallego <mysticallego@gmail.com>
2023-03-22 19:50:22 -0400

    Completed AutonPos3 template

--- src/main/java/frc/robot/commands/AutonPos3.java
+++ src/main/java/frc/robot/commands/AutonPos3.java
@@ -1,7 +1,22 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Intake;
 
 public class AutonPos3 extends SequentialCommandGroup {
-    
+    public AutonPos3(
+            Drivetrain drive,
+            Arm arm,
+            Intake intake) {
+        addCommands(
+
+            //...
+            new WaitCommand(1),
+            new InstantCommand(() -> drive.setBrakeMode(false), drive)
+        );
+    }
 }
