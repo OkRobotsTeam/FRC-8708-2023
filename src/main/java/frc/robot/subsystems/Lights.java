ae37bcfec26bc8fd7790ad84ae3c3c55f2b1d506
mysticallego <mysticallego@gmail.com>
2023-03-25 10:55:35 -0400

    Pulled color from driver station, unused right now

--- src/main/java/frc/robot/subsystems/Lights.java
+++ src/main/java/frc/robot/subsystems/Lights.java
@@ -1,9 +1,12 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Lights extends SubsystemBase {
+
+    
 
     public static final double violet = 0.91;
     public static final double yellow = 0.69;
@@ -11,10 +14,13 @@ public class Lights extends SubsystemBase {
     public static final double redchaser = -0.31;
     public static final double bluechaser = -0.29;
 
+    public static boolean isRed;
+
     public final PWMSparkMax LED = new PWMSparkMax(0);
 
     public Lights() {
         LED.set(off);
+        isRed = (DriverStation.getAlliance() == DriverStation.Alliance.Red); // Might always return false (DriverStation.Alliance.Invalid) if robot is not plugged in to field on initialization?
     }
 
     public void setViolet() {
